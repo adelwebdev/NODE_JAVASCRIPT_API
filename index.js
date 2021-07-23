fd785b854211076537a0ca19829edbf07aa25227
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-07-23 20:29:34 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const app = express();
+require("./models/dbconfig");
+const postsRoutes = require("./routes/postsController");
+
+app.use("/", postsRoutes);
 
 app.listen(5500, () => console.log("server started: 5500"));
