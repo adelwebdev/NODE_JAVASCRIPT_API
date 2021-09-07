63259ad74299446e1918d0999e79c0b131a827d3
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-09-07 21:54:17 +0300

    Add files via upload

--- models/postsModel.js
+++ models/postsModel.js
@@ -1,5 +1,9 @@
+// pr créer un modèle à la DB; créer fichier postsModel.js
+// d'abord appler mongooose
 const mongoose = require("mongoose");
 
+// ici modèle de DB qui sera stocké dans PostsModel
+// en premier: nom de la DB , en dernier: la table (ici posts), au milieu: tout ce qui a à l'intérieur
 const PostsModel = mongoose.model(
   "node-api",
   {
@@ -12,11 +16,15 @@ const PostsModel = mongoose.model(
       required: true,
     },
     date: {
-      type: Date,
+      type: Data,
       default: Date.now,
     },
   },
   "posts"
 );
 
+// aprés création du postsModel, il faut l'exporter (faire comme suite)
 module.exports = { PostsModel };
+
+// maintenant il nous faut des controllers! ; créer dossier routes (ou controllers)
+// dans routes ; créer postsController
