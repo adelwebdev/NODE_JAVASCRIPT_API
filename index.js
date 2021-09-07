265305571bc18baff2cb0dc3ae0cd57e0910ca20
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-09-07 21:53:26 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
+// on commence: appel de la console nmp: on tape; npm init -y (dire oui à tout) (cela crée le package.json)
+// ensuite on download express et nodemon ; npm i -s express nodeomon
+// express: framework nodeJS (diminue les lignes de codes lorsqu'on fait appel au server)
+// nodemon est un packet qui permet de mettre à jour le server à chaque enregistrement
+// dans package.json; on change "test": "echo \"Error: no test specified\" && exit 1" par "npm start": "nodemon index.js" pour la config de npm start
+// on fait: npm start ; pr faire tourner nodemon sur index.js
+
+// on appelle express; comme suite
 const express = require("express");
 const app = express();
-require("./models/dbconfig");
-const postsRoutes = require("./routes/postsController");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const cors = require("cors");
 
-mongoose.set("useFindAndModify", false);
+// de cette façon: on passe dbConfig à index.js
+require("./models/dbConfig");
 
-app.use(bodyParser.json());
-app.use(cors({ origin: "http://cdpn.io" }));
+// on veut que notre Router soit accessible dans l'index.js
+const postsRoutes = require("./routes/postsController.js");
+
+// on se crée un MIDDLEWARE (une fct qui va ecouter chaque changement sur req (request) et res (response))
+// surveille si l'appli est sur "/" alors envoie postsRoutes sachant que: const postsRoutes = require("./routes/postsController.js");
+// comme ceci les posts s'affichent dans localhost:5050/posts ; faut ajouter aussi le chemin entres " " dans : postsController; router.get(" ", (req,res))
 app.use("/posts", postsRoutes);
 
-app.listen(5500, () => console.log("server started: 5500"));
+// mainteant se connecter au server; avec express comme suite; on port 5500 tu écoutes se qui s'y passe
+// on insère un callBack; ici : () => {console.log("server started: 5500");}
+// il faut un DB, on crée des models; dans models: dbConfig.js
+app.listen(5500, () => {
+  console.log("server started: 5500");
+});
+
+// maintenant faut MongoDB et MongoDB Compass; le configurer et le lancer
+// on fait (dans compass) Create Database (possible d'insérer des data en brute "comme dans file mongo.db")
+// mainteant créer une connection à cette DB; la configurer! (voir models)
+// d'abord faut installer mongoose: npm i -s mongoose
